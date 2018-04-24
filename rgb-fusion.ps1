cbdf259bc3c8937a82e462225832d4fcdc0533b2
windows2000bug <38469063+windows2000bug@users.noreply.github.com>
2018-04-23 21:24:53 -0400

    Update rgb-fusion.ps1

--- rgb-fusion.ps1
+++ rgb-fusion.ps1
@@ -9,46 +9,63 @@
 #7 = Digital LED ?
 #8 = Digltal LED ?
 
-$games = "Overwatch","FarCry5"
+#Run once as it won't change
+$zones = & $path --list
+sleep 3
+
 $path = $PSScriptRoot + "\RGBFusionTool.exe"
+$games = (Get-Item $(dir $PSScriptRoot\profiles\*xml) ).Basename
+
 $c=0
 
 while ($true) {
-$x=0
-$z=0
-$y=0
-ForEach ($check in $games) {
-$process = get-process $check -ErrorAction SilentlyContinue
+    $x=0
+    $y=0
+    ForEach ($check in $games) {
+        $process = get-process $check -ErrorAction SilentlyContinue
+    
+        if ($process -ne $null ) {
+            write-host "Game Running, $check"           
+            [xml]$XmlDocument = Get-Content -Path $PSScriptRoot\profiles\$check.xml
 
-    if ($process -ne $null ) {
-        write-host "Game Running, $check"
-        if ($check -eq "Overwatch") {
-            $colors = "orange","white","orange","blue","blue"
-        } ElseIf ($check -eq "FarCry5") {
-            $colors = "blue","white","red","white","blue"
-        }
+            if ($c -eq 0) {
+                $m=0
+                foreach ($zone in $zones) {
 
-        if ($c -eq 0) {
-            ForEach ($setColor in $colors) {
-                write-host "changing color"
-                & $path -c  $setColor -z $z
-                $z+=1
-                #Sleeping exec commands to quickly can overload the buffer
-                sleep 1.25
+                    if ($zone  -like "*A_LED") {
+                        [long]$long = $XmlDocument.LED_info.Area_info[$m].But_Arg[0].startColor
+                        $hex = '{0:X}' -F $long
+                        $rgb = $hex.substring(2)
+
+                        #Gets Brightness from XML and turns into a percentage
+                        $bright = ([int]$XmlDocument.LED_info.Area_info[$m].Bri + 1) * 10
+                        write-host "zone" + $m
+                        & $path -z $m -c $rgb  -b $bright
+                        #Sleeping exec commands to quickly can overload the buffer
+                        sleep 2
+                    } elseif ($zone -like "*D_LED_TYPE_2") {
+                        #write-host "digital TBA"
+                    } elseif ($zone -like "*NA") {
+                        #Do nothing as no zone
+                    } else {
+                        write-host "shouldn't occur"
+                    }
+                    $m+=1
+                }
+                
+                $c=1
             }
-            $c=1
+        } Else {
+            $y+=1
         }
-    } Else {
-        $y+=1
+        $x+=1
     }
-    $x+=1
-}
 
-if ($x -eq $y ) {
-    write-host "No games running"
-    $c=0
-}
+    if ($x -eq $y ) {
+        write-host "No games running"
+        $c=0
+    }
 
-write-host "Sleeping to check for app running"
-sleep 3
+    write-host "Sleeping to check for app running"
+    sleep 3
 }
