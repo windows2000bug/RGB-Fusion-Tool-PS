1baa54257658b1b3253c8ce85f1bdc5b04054e56
windows2000bug <38469063+windows2000bug@users.noreply.github.com>
2018-05-01 20:41:45 -0400

    Update rgb-fusion.ps1

--- rgb-fusion.ps1
+++ rgb-fusion.ps1
@@ -14,6 +14,7 @@ $path = $PSScriptRoot + "\RGBFusionTool.exe"
 $zones = & $path --list
 sleep 3
 
+
 $games = (Get-Item $(dir $PSScriptRoot\profiles\*xml) ).Basename
 
 $c=0
@@ -27,34 +28,54 @@ while ($true) {
         if ($process -ne $null ) {
             write-host "Game Running, $check"           
             [xml]$XmlDocument = Get-Content -Path $PSScriptRoot\profiles\$check.xml
-
+            $build=""
             if ($c -eq 0) {
                 $m=0
                 foreach ($zone in $zones) {
-
                     if ($zone  -like "*A_LED") {
+                        #Get Color and turn it into RGB HEX
                         [long]$long = $XmlDocument.LED_info.Area_info[$m].But_Arg[0].startColor
                         $hex = '{0:X}' -F $long
                         $rgb = $hex.substring(2)
 
                         #Gets Brightness from XML and turns into a percentage
                         $bright = ([int]$XmlDocument.LED_info.Area_info[$m].Bri + 1) * 10
-                        write-host "zone" + $m
-                        & $path -z $m -c $rgb  -b $bright
-                        #Sleeping exec commands to quickly can overload the buffer
-                        sleep 2
+                        
+                        #Gets Pattern
+                        $pattern = [int]$XmlDocument.LED_info.Area_info[$m].Pattern_Type
+                        $speed = (50 / [int]$XmlDocument.LED_info.Area_info[$m].Speed)
+
+                        switch ( $pattern ) {
+                            #Static
+                            0 { $type=" --color="+$rgb+" --brightness="+$bright }
+                            #Pulse
+                            1 { $type=" --pulse="+$rgb+" --fadeon="+$speed+" --fadeoff="+$speed+" --maxbrightness="+$bright+" --minbrightness=0" }
+                            #Flash
+                            #4 { $type=" --flash="+$rgb+"  --count=1 --interval=10 --maxbrightness="+$bright+" --minbrightness=0" } #--flashcycle=1  --count=1 --time=1 --interval=4
+                            #Color Cycle
+                            9 { $type=" --cycle="+$speed+" --brightness="+$bright }
+                            #Double Flash
+                            #11 { $type="double flash" }
+                            #Failsafe
+                            default { $type = " --color="+$rgb+" --brightness="+$bright }
+                        }
+                        
+                        $build += " --zone="+$m+$type
                     } elseif ($zone -like "*D_LED_TYPE_2") {
                         #write-host "digital TBA"
                     } elseif ($zone -like "*NA") {
-                        #Do nothing as no zone
+                        #Do nothing as no zone exists
                     } else {
                         write-host "shouldn't occur"
                     }
                     $m+=1
                 }
-                
+
+
+                & $path $build.split(" ")
+                write-host $path $build.split(" ")
                 $c=1
-            }
+           }
         } Else {
             $y+=1
         }
